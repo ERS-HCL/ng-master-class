10ac2bf377a8f67cd0c8e05e269b207f0d8a6b01
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-10-04 12:00:52 +0530

    Added Auth guard

--- apps/customer-portal/src/app/app.router.ts
+++ apps/customer-portal/src/app/app.router.ts
@@ -4,19 +4,25 @@ import { AppLoginComponent } from './app-login/app-login.component';
 import { CheckOutPageComponent } from './check-out-page/check-out-page.component';
 import { AboutComponent } from '@hcl-ers/core-ui';
 import { ProductsPageComponent } from './products-page/products-page.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const router: Routes = [
   { path: '', redirectTo: 'products', pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
-  { path: 'products', component: ProductsPageComponent },
+  {
+    path: 'products',
+    component: ProductsPageComponent
+  },
   { path: 'login', component: AppLoginComponent },
   {
     path: 'user-registration',
-    loadChildren: '@hcl-ers/user-registration#UserRegistrationModule'
+    loadChildren: '@hcl-ers/user-registration#UserRegistrationModule',
+    canActivate: [AuthGuard]
   },
   {
     path: 'check-out',
-    component: CheckOutPageComponent
+    component: CheckOutPageComponent,
+    canActivate: [AuthGuard]
   }
 ];
 

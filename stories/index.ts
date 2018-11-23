b629efc55eda07a8dfaded1e0d44047ad53d51ef
Tarun Kumar Sukhu - ERS <tsukhu@hcl.com>
2018-11-23 16:51:08 +0530

    Fixed the material icons issue

--- stories/index.ts
+++ stories/index.ts
@@ -3,13 +3,7 @@ import { storiesOf } from '@storybook/angular';
 
 storiesOf('Chapter 1: Overview', module)
 .add('Introduction', () => ({
-    template: `<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <style>
-.base {
-    margin: 10px;
-    font-family: 'Roboto', sans-serif;
-}
-</style>
+    template: `
     <div class="base">
       <h1>Customer Portal</h1>
       <p>This is a living style guide</p>
